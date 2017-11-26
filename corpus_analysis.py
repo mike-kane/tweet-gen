b00df74b77ecc663dcf1b56e07a7a41536e5702e
Michael Kane <mediocrecomedy@gmail.com>
2017-11-26 15:09:10 -0800

    initial commit.  Contains method stubs

---
+++ corpus_analysis.py
@@ -0,0 +1,41 @@
+
+class Analyzer(object):
+
+
+
+    def __init__(self, histogram):
+        self.histogram = histogram
+
+    def min_vals(num_vals=10):
+        '''
+        Returns all tokens with lowest number of appearances in the corpus. For
+        tokens that have the same values, will return all tokens with that value.
+
+        Users may specify the number of minimum values to be returned.  Default is 10.
+        '''
+        pass
+
+    def max_vals(num_vals=10):
+        '''
+        Returns all tokens with highest number of appearances in the corpus. For
+        tokens that have the same values, will return all tokens with that value.
+
+        Users may specify the number of maximum values to be returned.  Default is 10.
+        '''
+        pass
+
+    def median_val():
+        '''
+        Returnsa tuple containing the token(s) with the median number of appearances across the
+        entire corpus, as well as the median value.
+        '''
+        pass
+
+    def mode_val():
+        '''
+        Returnsa tuple containing the token(s) with the mode value of appearances across the
+        entire corpus, as well as the mode value.
+        '''
+        pass
+
+    
